facf2bdb4718c864feabaf947cfb0e9b8769e55c
Bri Han <bri@bimodalconsulting.com>
2023-12-14 14:18:13 -0800

    updating

--- SurfsUP/app.py
+++ SurfsUP/app.py
@@ -69,6 +69,64 @@ def precipitation():
 # 3. /api/v1.0/stations. Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def stations():
+    results = session.query(Station.station).group_by(Station.station).all()
+    stations_list = list(np.ravel(results))
+    resturn jsonify(stations_list)
+
+
+# 4. /api/v1.0/tobs. Query the dates and temperature observations of the most-active station for the previous year of data. 
+# Return a JSON list of temperature observations for the previous year.
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    one_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    year_results = session.query(Measurement.tobs).\
+        filter(Station.station == 'USC00519281').\
+        filter(Measurement.date >= one_year).all()
+    temp = list(np.ravel(year_results))
+
+    return jsonify(temp)
+
+
+# 5. /api/v1.0/<start> and /api/v1.0/<start>/<end>. Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+
+@app.route("/api/v1.0/<start>")
+def temps_start():
+    session = Session(engion)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+    filter(Measurement.date >= start).all()
+    
+    session.close()
+
+    temps = []
+    for min_temp, avg_temp, max_temp in results:
+        temps_dict = {}
+        temps_dict['Min Temp'] = min_temp
+        temps_dict['Avg Temp'] = avg_temp
+        temps_dict['Max Temp'] = max_temp
+        temps.append(temps_dict)
+
+    return jsonify(temps)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temps_start_end():
     session = Session(engine)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    session.close()
+
+    temps = []
+    for temp_min, temp_avg, temp_max in results:
+        temps_dict = {}
+        temps_dict["Min Temp"] = min_temp
+        temps_dict["Avg Temp"] = avg_temp
+        temps_dict["Max Temp"] = max_temp
+        temps.append(temps_dict)
+
+    return jsonify(temps)
+
     
 
